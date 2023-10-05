0ce5ed098f99779ea3771932b7a3389a8bfe2361
Mateboup <mbotero@qvision.com.co>
2023-10-04 21:51:39 -0500

    Create the runner with serenity and cucumber

--- src/test/java/com/dahia/runners/LoginSauceDemo.java
+++ src/test/java/com/dahia/runners/LoginSauceDemo.java
@@ -1,4 +1,10 @@
 package com.dahia.runners;
 
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/feature/login_sauce_demo.feaure")
 public class LoginSauceDemo {
 }
