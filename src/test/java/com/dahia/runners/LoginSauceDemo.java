b18e1466f5bd4f765027b3e2eee47bec38afee90
Mateboup <mbotero@qvision.com.co>
2023-10-04 22:09:19 -0500

    Steps

--- src/test/java/com/dahia/runners/LoginSauceDemo.java
+++ src/test/java/com/dahia/runners/LoginSauceDemo.java
@@ -5,6 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/feature/login_sauce_demo.feaure")
+@CucumberOptions(features = "src/test/resources/feature/login_suace_demo.feature",
+glue = "src.test.java.com.dahia.stepdefinitions")
 public class LoginSauceDemo {
 }
--- src/test/java/com/dahia/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/com/dahia/stepdefinitions/LoginStepDefinitions.java
@@ -1,4 +1,26 @@
 package com.dahia.stepdefinitions;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class LoginStepDefinitions {
+
+    @Given("the user is in the page")
+    public void the_user_is_in_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user enter the credentials")
+    public void the_user_enter_the_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the user can enter")
+    public void the_user_can_enter() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 }
