081fd3cd1d34066af4586fa9422fd62122fa2c56
dahiana <dasaro96@hotmail.com>
2023-10-04 21:05:30 -0500

    Full automation with Selenium

--- src/test/java/com/dahia/Login.java
+++ src/test/java/com/dahia/Login.java
@@ -1,23 +1,49 @@
 package com.dahia;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class Login {
 
     @Test
-    public void loginInOrangeHrm() {
+    public void loginInOrangeHrm()  {
         // Configurar la ubicación del controlador de Chrome (webdriver)
-        System.setProperty("web-driver.chrome.driver","chrome.exe");
+        //System.getProperty();
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
         // Inicializar el navegador
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver,10);
 
         // Abrir una página web
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.get("https://opensource-demo.orangehrmlive.com/");
 
-        // Realizar acciones en la página (por ejemplo, hacer clic en un enlace, llenar un formulario, etc.)
+        //Buscar elemento
+        WebElement userName = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()=' Login ']"));
+        String userValidation = "Joa Caseres";
+
+        // Realizar acciones
+        userName.sendKeys("Admin");
+        password.sendKeys("admin123");
+        loginButton.click();
+
+        //Mapeo
+        WebElement loginValidation = wait.until(ExpectedConditions.presenceOfElementLocated
+                (By.xpath("//p[@class='oxd-userdropdown-name']")));
+
+        assertTrue(loginValidation.isEnabled());
+        System.out.println(loginValidation.getText());
 
         // Cerrar el navegador
         driver.quit();
